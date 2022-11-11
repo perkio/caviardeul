4b9e42adfcc8ff2196ccc0b971fd9b9810fca4d1
amper <amper@screenovate.com>
2022-11-11 15:03:09 +0200

    font wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,23 @@ import dynamic from "next/dynamic";
 import "nprogress/nprogress.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Analytics } from '@vercel/analytics/react';
+import { Roboto, Rubik, Miriam_Libre } from "@next/font/google"
+
+// const roboto = Roboto({
+//   weight: '400',
+// })
 
 import Layout from "@caviardeul/components/layout";
-
 import "../styles/style.scss";
+
+const font = Miriam_Libre({
+  variable: '--puzzle-font',
+  weight: "400",
+  subsets: ["hebrew"]
+})
+
+console.log('aa', font.variable)
+
 
 const queryClient = new QueryClient();
 
@@ -23,7 +36,9 @@ const Caviardeul = ({ Component, pageProps }: AppProps) => {
       <TopProgressBar />
       <QueryClientProvider client={queryClient}>
         <Layout>
-          <Component {...pageProps} />
+          <div className={font.variable}>
+            <Component {...pageProps} />
+          </div>
         </Layout>
       </QueryClientProvider>
       <Analytics />
